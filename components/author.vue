a3206057a89cbd4299aaf69b3952ebfe371b6c6c
yagisuke <yagisuke.0209@gmail.com>
2018-07-07 23:58:05 +0900

    styling

---
+++ components/author.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <a href="https://twitter.com/yagi_suke" target="_blank">
+      <img src="/yagisuke.jpg" alt="yagisuke" class="author">
+    </a>
+  </div>
+</template>
+
+<style>
+.author {
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  animation: fuwafuwa 3s infinite linear alternate;
+}
+
+@keyframes fuwafuwa {
+  0% {
+    transform:translate(0, 0) rotate(-5deg);
+  }
+  50% {
+    transform:translate(0, -5px) rotate(0deg);
+  }
+  100% {
+    transform:translate(0, 0)rotate(5deg);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
-      <h1 class="title">firebase-hosting</h1>
-      <h2 class="subtitle">My good Nuxt.js project</h2>
+      <author/>
+      <h1 class="title">@yagi_suke</h1>
       <div class="links">
-        <a href="https://github.com/yagisuke/firebase-hosting/blob/master/README.md" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/yagisuke/firebase-hosting" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
@@ -13,43 +11,34 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Author from '~/components/author.vue'
 
 export default {
   components: {
-    Logo
+    Author
   }
 }
 </script>
 
 <style>
-.container
-{
+.container {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
-.title
-{
+
+.title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 50px;
   color: #35495e;
   letter-spacing: 1px;
 }
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
+
+.links {
   padding-top: 15px;
 }
 </style>
